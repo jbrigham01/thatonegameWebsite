cdbac18d35099a03320c3821a133500cc38f7db3
Jeremy Brigham <jbrigham@Jeremys-MacBook-Pro.local>
2024-12-28 00:54:27 -0700

    updating, adding mongoDB

--- togwebsite/src/App.jsx
+++ togwebsite/src/App.jsx
@@ -1,8 +1,6 @@
 import { useState } from 'react'
-import Navbar from './components/Navbar'
-import Smileybutton from './components/smileybutton'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from './components/Navbar.jsx'
+import Smileybutton from './components/Smileybutton.jsx'
 import './App.css'
 
 function App() {
@@ -14,18 +12,15 @@ function App() {
       <h1>What is ThatOneGame?</h1>
       <h2>It's the best name for this game I could come up with..</h2>
       <h3>But besides that...!</h3>
-
       <div style={{"display":"float",width:"60vw", "flexDirection":"row", 'marginLeft':'auto','marginRight':'auto'}}>
       <img src="./src/assets/intro1.png"></img>
       <img src="./src/assets/intro2.png"></img>
       </div>
-      
       <p></p>
       <h2>It's an action rpg for Mac/PC!</h2>
       <div style={{"display":"float",width:"60vw", "flexDirection":"row", 'marginLeft':'auto','marginRight':'auto'}}>
       <img src="./src/assets/fight1.png"></img>
       <img src="./src/assets/fight2.png"></img>
-      
       </div>
       <h2>Where you fight things!</h2>
     </div>
--- togwebsite/src/components/Navbar.jsx
+++ togwebsite/src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react'
-import "./navbar.css"
+import "./Navbar.css"
 const Navbar = () => {
     return  (
     <Fragment>
